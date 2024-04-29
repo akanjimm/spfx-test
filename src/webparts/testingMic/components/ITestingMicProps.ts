e1deae7f6c516661adec0e16dc92c4b4d978ed9e
Mohammed Akanji <akanjimm@gmail.com>
2024-04-29 23:01:01 +0100

    initial commit

---
+++ src/webparts/testingMic/components/ITestingMicProps.ts
@@ -0,0 +1,7 @@
+export interface ITestingMicProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/testingMic/components/TestingMic.tsx
@@ -0,0 +1,22 @@
+// import * as React from 'react';
+// import styles from './TestingMic.module.scss';
+// import type { ITestingMicProps } from './ITestingMicProps';
+// import { escape } from '@microsoft/sp-lodash-subset';
+
+// export default class TestingMic extends React.Component<ITestingMicProps, {}> {
+//   public render(): React.ReactElement<ITestingMicProps> {
+//     const {
+//       description,
+//       isDarkTheme,
+//       environmentMessage,
+//       hasTeamsContext,
+//       userDisplayName
+//     } = this.props;
+
+//     return (
+//       <section className={`${styles.testingMic} ${hasTeamsContext ? styles.teams : ''}`}>
+//         How are you Seyi!
+//       </section>
+//     );
+//   }
+// }
